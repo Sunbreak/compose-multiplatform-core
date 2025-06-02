50aad176d573e6d6899d9888617f110619d88ae2
Levi Albuquerque <levima@google.com>
2025-06-02 11:25:56 +0100

    Use CacheWindow in Pager
    Use CacheWindow in Pager. Pager will now use Cache Window to prefetch and cache pages, instead of a single page in the direction of the scroll. This should work in addition to beyond bounds page count.
    
    Relnote: Update Pager's prefetch strategy to use Cache Window instead of the default 1 item in the direction of the scroll. Now initial prefetching is enabled by default in Pager and prefetching will be based on the size of the window worth 1 viewport, that is, we will try to fill 1 whole view port with prefetched items. Items will be kept around longer as well.
    
    Test: Added new tests.
    
    Fixes: 315280218
    Fixes: 292136289
    Change-Id: I4d45ece5bab78dc241618d29d3701e5463789aa6

--- compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/ComposeFoundationFlags.kt
+++ compose/foundation/foundation/src/commonMain/kotlin/androidx/compose/foundation/ComposeFoundationFlags.kt
@@ -160,4 +160,11 @@ object ComposeFoundationFlags {
      * element on the screen.
      */
     @Suppress("MutableBareField") @JvmField var isTextFieldDpadNavigationEnabled = true
+
+    /**
+     * With this flag on, Pager will use Cache Window as the default prefetching strategy, instead
+     * of 1 item in the direction of the scroll. The window used will be 1 view port AFTER the
+     * currently composed items, this includes visible and items composed through beyond bounds.
+     */
+    @Suppress("MutableBareField") @JvmField var isCacheWindowForPagerEnabled = true
 }
